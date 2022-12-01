2a993135fb508f030b78230f28bfffe11d80e649
eNgine9r <92324923+eNgine9r@users.noreply.github.com>
2022-12-01 21:25:48 +0000

    Deploying to gh-pages from @ eNgine9r/team-proj-test@999967500e50044fc32762dee1064fd4886e0e40 🚀

---
+++ index.560c807f.js
@@ -0,0 +1,2 @@
+new Swiper(".image-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.560c807f.js.map
---
+++ index.bba5b993.js
@@ -0,0 +1,2 @@
+new Swiper(".image-slider",{navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.bba5b993.js.map
